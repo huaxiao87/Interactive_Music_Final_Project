a44da947390e0b9d0ce4823043870789e5e110ac
Hua Xiao <shawn.huaxiao@gmail.com>
2015-12-10 01:21:15 -0500

    updates

--- startGF.js
+++ startGF.js
@@ -1,22 +1,71 @@
 inlets	=3;
-outlets	=5
+outlets	=3;
 
-var lastX	= 0;//initialization
-var lastY 	= 0;
- 
-function list(){
+var currentPoint		= new Array(2);
+var lastPoint			= new Array(2);//for calculating the speed
+var startPoint			= new Array(2);//for calculating the relative position
+var relativePosition	= new Array(2);
+
+
+var isSending 			= 0;
+var distanceThreshold	= 20;
+
+function list()
+{
+	if ( input==0 )
+	{
+		currentPoint[0]		= arguments[0];
+		currentPoint[1] 	= arguments[1];
 	
-	x	= arguments[0];
-	y 	= arguments[1];
-	
-	//check the distance between current list and last list
-	distance 	= norm(x - lastX, y - lastY);
-	
-	outlet(0,norm(x,y));
+		//check the distance between current list and last list
+		distance 	= norm(x - lastPoint[0], y - lastPoint[1]);
+		
+		if (distance > distanceThreshold )
+		{
+			if ( isSending == 0 )
+			{
+				isSending 	= 1;
+				startPoint	= currentPoint;
+				outlet(2,1);
+				
+				startTiming();//after a period, stop sending messages
+				sendRelativePosition( x,y );
+				lastPoint
+			}
+			else
+			{
+				sendRelativePosition( x,y );
+			}
+		}
+		else 
+		{
+			isSending == 1;
+			sendRelativePosition( x,y );
+		}
+	}
 }
 
 // detect the speed
-function norm( x, y )
-{
+function norm( x, y ){
 		return Math.sqrt(x*x+y*y);
+}
+
+function sendRelativePosition( x,y ){
+	position[0] 	= x-startPoint[0];
+	position[1] 	= y-startPoint[1];
+	outlet(0,position);
+}
+
+function startTiming()
+{
+	outlet(1,1);
+}
+
+function msg_int()
+{
+	if ( input==1 && arguments[0]==0 )
+	{
+		isSending = 0;
+		outlet(2,0);
+	}
 }
---
+++ test.js
@@ -0,0 +1,12 @@
+inlets 	=2;
+outlets =2;
+
+var position	= new Array(2);
+
+function msg_float(){
+	position[0]		=2;
+	position[1]		=1;
+	outlet(0,position);
+}
+	
+
